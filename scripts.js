7df9a10709c11a8a3ed195525ed475ff60a27ebe
YoSuhWOF <110609997+KansasCityWX@users.noreply.github.com>
2022-11-12 12:56:45 -0600

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("navbar").style.width = "10.75rem";
+
+  }
+  
+  function closeNav() {
+      document.getElementById("navbar").style.width = "0rem";
+  }
